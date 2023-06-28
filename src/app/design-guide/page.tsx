4acd3ba0e10206282604e2be559f9b06febc4531
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-28 16:10:39 +0200

    create design guide page

---
+++ src/app/design-guide/page.tsx
@@ -0,0 +1,11 @@
+import Buttons from "@/components/design-guide/Buttons";
+import Inputs from "@/components/design-guide/Inputs";
+
+export default function DesignGuide() {
+  return (
+    <main>
+      <Buttons />
+      <Inputs />
+    </main>
+  );
+}
