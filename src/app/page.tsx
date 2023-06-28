261be8c5474ddfdd89ba92b107decb826baa5e89
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-28 19:21:31 +0200

    update layout to include roadmap counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import Actionbar from "@/components/Actionbar";
 import FeedbackItem from "@/components/FeedbackItem";
+import RoadmapCounter from "@/components/RoadmapCounter";
 import Buttons from "@/components/design-guide/Buttons";
 import Inputs from "@/components/design-guide/Inputs";
 
 export default function Home() {
   return (
-    <>
-      <header className="mb-4">
-        <Actionbar />
-      </header>
-      <main className="container space-y-4">
+    <div className="flex gap-6">
+      <aside className="w-1/4">
+        <RoadmapCounter />
+      </aside>
+      <main className="space-y-4 flex-grow">
+        <header className="mb-4 tablet:mb-6">
+          <Actionbar />
+        </header>
         <FeedbackItem />
         <FeedbackItem />
         <FeedbackItem />
       </main>
-    </>
+    </div>
   );
 }
