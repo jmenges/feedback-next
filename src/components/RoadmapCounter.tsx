dc366352a9e3219973f2c3f1dcd208cb698c5716
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-28 19:21:13 +0200

    create initial RoadmapCounter component

---
+++ src/components/RoadmapCounter.tsx
@@ -0,0 +1,34 @@
+import Card from "@/components/Card";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type RoadmapCounterProps = {};
+
+const roadMaps = [
+  { title: "Planned", color: "orange" },
+  { title: "In-Progress", color: "purple" },
+  { title: "Live", color: "bright-blue" },
+];
+
+export default function RoadmapCounter({}: RoadmapCounterProps) {
+  return (
+    <Card>
+      <div className="mb-6 flex justify-between">
+        <h2>Roadmap</h2>
+        <Button size="raw" variant="link">
+          View
+        </Button>
+      </div>
+      {/* ROADMAPS */}
+      <div className="space-y-2">
+        {roadMaps.map((roadMap) => (
+          <div className="flex items-center">
+            <i className={`mr-2 h-2 w-2 rounded-full bg-${roadMap.color}`} />
+            <span className="mr-auto">{roadMap.title}</span>
+            <span className="font-bold">9</span>
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
+}
