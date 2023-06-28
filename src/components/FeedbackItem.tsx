1144569ef7eafd89c4775e158cfdb8daa30d4f6f
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-28 18:49:01 +0200

    use card component

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,3 +1,4 @@
+import Card from "@/components/Card";
 import CommentCounter from "@/components/ui/CommentCounter";
 import Tag from "@/components/ui/Tag";
 import UpvoteButton from "@/components/ui/UpvoteButton";
@@ -7,17 +8,22 @@ type Props = {};
 
 export default function FeedbackItem({}: Props) {
   return (
-    <article className="flex flex-wrap items-start rounded-md bg-white p-6 tablet:px-8 tablet:py-7">
+    <Card className="flex flex-wrap items-start">
       {/* TITLE, DESCRIPTION, TAG */}
-      <div className="mb-4 tablet:mb-0 space-y-2 tablet:order-2 w-full tablet:w-auto">
+      <div className="mb-4 w-full space-y-2 tablet:order-2 tablet:mb-0 tablet:w-auto">
         <h4 className="tablet:text-h3">Add tags for solutions</h4>
-        <p className="">Easier to search for solutions based on a specific stack.</p>
+        <p className="">
+          Easier to search for solutions based on a specific stack.
+        </p>
         <Tag title="Enhancement" type="info" />
       </div>
       {/* UPVOTE BUTTON */}
-        <UpvoteButton className="tablet:order-1 tablet:mr-10" />
-        <CommentCounter className="ml-auto tablet:self-center tablet:order-3" count={0} />
+      <UpvoteButton className="tablet:order-1 tablet:mr-10" />
+      <CommentCounter
+        className="ml-auto tablet:order-3 tablet:self-center"
+        count={0}
+      />
       {/* COMMENT COUNT */}
-    </article>
+    </Card>
   );
 }
