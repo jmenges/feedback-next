c608f7fd54c2726bdc8b03eba5d7e5a2f61d5737
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-28 18:48:50 +0200

    create card component used to wrap various components

---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+type CardProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export default function Card({ children, className }: CardProps) {
+  return (
+    <div
+      className={cn(
+        className,
+        "rounded-md bg-white p-6 tablet:px-8 tablet:py-7"
+      )}
+    >
+      {children}
+    </div>
+  );
+}
