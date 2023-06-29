57ed2353acc20f874f47171ef941a43bfdb16dd9
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-29 14:07:44 +0200

    update home page to include new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Actionbar from "@/components/Actionbar";
+import AppCard from "@/components/AppCard";
 import FeedbackItem from "@/components/FeedbackItem";
 import RoadmapCounter from "@/components/RoadmapCounter";
-import Buttons from "@/components/design-guide/Buttons";
-import Inputs from "@/components/design-guide/Inputs";
+import TagFilter from "@/components/TagFilter";
 
 export default function Home() {
   return (
-    <div className="flex gap-6">
-      <aside className="w-1/4">
+    <div className="flex flex-wrap gap-6">
+      <aside className="w-full desktop:w-1/4 space-y-6">
+        <AppCard />
+        <TagFilter />
         <RoadmapCounter />
       </aside>
       <main className="space-y-4 flex-grow">
