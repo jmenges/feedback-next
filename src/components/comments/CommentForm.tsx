119a0932555ffe406a162316bc9c1ad8fc3c9d8e
Jonas Menges <dev.menges.jonas@gmail.com>
2023-06-29 14:22:42 +0200

    change input for textarea in comment form

--- src/components/comments/CommentForm.tsx
+++ src/components/comments/CommentForm.tsx
@@ -1,7 +1,6 @@
 import Card from "@/components/Card";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import React from "react";
+import { Textarea } from "@/components/ui/textarea";
 
 type Props = {};
 
@@ -9,7 +8,7 @@ export default function CommentForm({}: Props) {
   return (
     <Card>
       <h2 className="mb-6">Add Comment</h2>
-      <Input className="mb-4" placeholder="Type your comment here" />
+      <Textarea className="mb-4" placeholder="Type your comment here" />
       <div className="flex justify-between">
         <span>250 Characters left</span><Button variant="default">Post Comment</Button>
       </div>
