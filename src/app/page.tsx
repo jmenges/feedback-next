affd1cbbf1b55d5a52eddc97eb0f5fc4b9f9aa9b
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-03 15:26:40 +0200

    Use FeedbackList instead of individual FeedbackItems

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
 import Actionbar from "@/components/Actionbar";
 import AppCard from "@/components/AppCard";
-import FeedbackItem from "@/components/FeedbackItem";
+import FeedbackList from "@/components/FeedbackList";
 import RoadmapCounter from "@/components/RoadmapCounter";
 import TagFilter from "@/components/TagFilter";
 
 export default function Home() {
   return (
     <div className="flex flex-wrap gap-6">
-      <aside className="w-full desktop:w-1/4 space-y-6">
+      <aside className="w-full space-y-6 desktop:w-1/4">
         <AppCard />
         <TagFilter />
         <RoadmapCounter />
       </aside>
-      <main className="space-y-4 flex-grow">
+      <main className="flex-grow">
         <header className="mb-4 tablet:mb-6">
           <Actionbar />
         </header>
-        <FeedbackItem />
-        <FeedbackItem />
-        <FeedbackItem />
+        <FeedbackList />
       </main>
     </div>
   );
