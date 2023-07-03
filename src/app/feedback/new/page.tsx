ef8873ec7320ad11fe29847af9cf3a7e7eca9953
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-03 15:26:10 +0200

    Update feedback add page to generate the correct path for the BackButton inside the Header and Form

--- src/app/feedback/new/page.tsx
+++ src/app/feedback/new/page.tsx
@@ -1,15 +1,24 @@
 import NewFeedbackForm from "@/components/feedback-form/NewFeedbackForm";
 import BackButton from "@/components/ui/BackButton";
 
+import { headers } from "next/headers";
+import Link from "next/link";
+
 export default function NewFeedback() {
+  const headersList = headers();
+  const domain = headersList.get("host") || "";
+  const referer = headersList.get("referer");
+  const path = referer?.split(domain)[1] || "";
+  const backPath = path === "/feedback/new" ? "/" : path;
+
   return (
     <div className="mt-6 flex flex-col gap-6 tablet:mt-0">
       {/* Actions */}
-      <div className="flex justify-between mb-10">
-        <BackButton variant="link" />{" "}
+      <div className="mb-10 flex justify-between">
+         <BackButton variant="link" href={backPath} />
       </div>
       {/* Feedback Form */}
-      <NewFeedbackForm />
+      <NewFeedbackForm cancelHref={backPath}/>
     </div>
   );
 }
