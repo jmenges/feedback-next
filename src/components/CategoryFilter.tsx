f061f84678df699c7d388cfbec9470fdc96f9a86
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-05 13:14:31 +0200

    rename tags as categories

---
+++ src/components/CategoryFilter.tsx
@@ -0,0 +1,28 @@
+import Card from "@/components/Card";
+import Category from "@/components/ui/Category";
+import { ICategory, categories } from "@/data/categories";
+import React from "react";
+
+type CategoryFilterProps = {
+  activeCategory: ICategory;
+  setActiveCategory: React.Dispatch<ICategory>;
+};
+
+export default function CategoryFilter({
+  activeCategory,
+  setActiveCategory,
+}: CategoryFilterProps) {
+  return (
+    <Card className="flex flex-wrap gap-x-2 gap-y-2">
+      {categories.map((category, index) => (
+        <Category
+          key={index}
+          title={category}
+          type="button"
+          active={category === activeCategory}
+          onClick={() => setActiveCategory(category)}
+        />
+      ))}
+    </Card>
+  );
+}
