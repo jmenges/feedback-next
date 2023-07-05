b1a1e9aca7e72463937f893e516c8bac7622849c
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-05 20:11:24 +0200

    Add aria-invalid styling

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full rounded-sm border border-transparent bg-lighter-grey px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:border-blue  disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[60px] w-full rounded-sm border border-transparent bg-lighter-grey px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:border-blue  disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-red",
           className
         )}
         ref={ref}
