84316bf5bd3f48b9ccb58bfd553f9dce316ba961
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-06 10:41:29 +0200

    Pass correct parameters to EditFeedbackForm

--- src/app/feedback/[id]/edit/page.tsx
+++ src/app/feedback/[id]/edit/page.tsx
@@ -1,15 +1,24 @@
 import EditFeedbackForm from "@/components/feedback-form/EditFeedbackForm";
 import BackButton from "@/components/ui/BackButton";
+import { headers } from "next/headers";
 
-export default function NewFeedback() {
-  return (
-    <div className="mt-6 flex flex-col gap-6 tablet:mt-0">
-      {/* Actions */}
-      <div className="flex justify-between mb-10">
-        <BackButton variant="link" />{" "}
-      </div>
-      {/* Feedback Form */}
-      <EditFeedbackForm />
+export default function EditFeedback({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const headersList = headers();
+  const domain = headersList.get("host") || "";
+  const referer = headersList.get("referer");
+  const path = referer?.split(domain)[1] || "";
+  const backPath = path === "" || path.endsWith("/edit") ? "/" : path;
+
+  <div className="mt-6 flex flex-col gap-6 tablet:mt-0">
+    {/* Actions */}
+    <div className="mb-10 flex justify-between">
+      <BackButton variant="link" href={backPath} />
     </div>
-  );
+    {/* Feedback Form */}
+    <EditFeedbackForm feedbackId={Number(id)} returnHref={backPath} />
+  </div>;
 }
