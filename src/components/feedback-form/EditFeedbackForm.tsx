3d4f3888ae2e35987ae2424a48681901095b53c7
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-06 10:37:36 +0200

    Implement logic for handling feedback edit form

--- src/components/feedback-form/EditFeedbackForm.tsx
+++ src/components/feedback-form/EditFeedbackForm.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import IconEditFeedback from "@/../public/icons/icon-edit-feedback.svg";
-import FeedbackForm from "./FeedbackForm";
-import { Button } from "@/components/ui/button";
-import { IFeedbackPartial } from "@/types";
-import Link from "next/link";
+"use client";
 
-type EditFeedbackFormProps = { feedback: IFeedbackPartial; cancelHref: string };
+import IconEditFeedback from "@/../public/icons/icon-edit-feedback.svg";
+import { Button } from "@/components/ui/button";
+import { useFeedbackStore } from "@/store/useFeedbackStore";
+import { IEditFeedback } from "@/types";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import FeedbackForm from "./FeedbackForm";
+
+type EditFeedbackFormProps = { feedbackId: number; returnHref: string };
 
 const FeedbackActions = ({ cancelHref }: { cancelHref: string }) => {
   return (
@@ -23,9 +27,41 @@ const FeedbackActions = ({ cancelHref }: { cancelHref: string }) => {
   );
 };
 
-export default function EditFeedbackForm({ feedback, cancelHref }: EditFeedbackFormProps) {
-  const onSubmit = (data) => {
-    console.log(data);
+export default function EditFeedbackForm({
+  feedbackId,
+  returnHref,
+}: EditFeedbackFormProps) {
+  const {
+    feedbacks,
+    getById: getFeedbackById,
+    editFeedback,
+  } = useFeedbackStore();
+  const [feedback, setFeedback] = useState<IEditFeedback | undefined>(
+    undefined
+  );
+  const router = useRouter();
+
+  useEffect(() => {
+    const feedback = getFeedbackById(feedbackId);
+    const inputFeedback: IEditFeedback | undefined =
+      feedback !== null
+        ? {
+            id: feedback.id,
+            title: feedback.title,
+            status: feedback.status,
+            description: feedback.description,
+            category: feedback.category,
+          }
+        : undefined;
+    setFeedback(inputFeedback);
+  }, [feedbacks, feedbackId]);
+
+  const onSubmit = (data: IEditFeedback) => {
+    const isSuccess = editFeedback(data);
+    console.log(isSuccess);
+    if (isSuccess === true) {
+      router.push(returnHref);
+    }
   };
 
   return (
@@ -34,7 +70,7 @@ export default function EditFeedbackForm({ feedback, cancelHref }: EditFeedbackF
       feedback={feedback}
       Icon={IconEditFeedback}
       title="Editing ‘Add a dark theme option’"
-      Actions={() => <FeedbackActions cancelHref={cancelHref} />}
+      Actions={() => <FeedbackActions cancelHref={returnHref} />}
     />
   );
 }
