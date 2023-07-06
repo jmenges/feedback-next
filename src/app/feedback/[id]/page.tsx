7f0c6a3ac64c315324d613369d86c30677385d2d
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-06 10:37:12 +0200

    Use real data in feedback detail page

--- src/app/feedback/[id]/page.tsx
+++ src/app/feedback/[id]/page.tsx
@@ -1,19 +1,37 @@
+"use client";
+
 import FeedbackItem from "@/components/FeedbackItem";
 import CommentForm from "@/components/comments/CommentForm";
 import CommentList from "@/components/comments/CommentList";
 import BackButton from "@/components/ui/BackButton";
 import { Button } from "@/components/ui/button";
+import { useFeedbackStore } from "@/store/useFeedbackStore";
+import Link from "next/link";
 
-export default function FeedbackDetail() {
+export default function FeedbackDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const { getById, upvoteFeedback } = useFeedbackStore();
+  const feedback = getById(Number(id));
+
+  if (!feedback) return;
+
+  // const backHref = genBackLink("")
+
   return (
     <div className="mt-6 flex flex-col gap-6 tablet:mt-0">
       {/* Actions */}
       <div className="flex justify-between">
-        <BackButton variant="link" />{" "}
-        <Button variant="accent">Edit Feedback</Button>
+        <BackButton variant="link" href="/" />
+
+        <Button variant="accent" asChild>
+          <Link href={`/feedback/${feedback.id}/edit`}>Edit Feedback</Link>
+        </Button>
       </div>
       {/* Feedback Card */}
-      <FeedbackItem />
+      <FeedbackItem upvoteFeedback={upvoteFeedback} feedback={feedback} />
       {/* Comments */}
       <CommentList />
       <CommentForm />
