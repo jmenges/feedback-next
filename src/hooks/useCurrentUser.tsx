76e10fba4835110e5a1e993238d765d2595988f7
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-07 11:15:30 +0200

    Create mock custom hook for current user

---
+++ src/hooks/useCurrentUser.tsx
@@ -0,0 +1,16 @@
+import { IUser } from "@/types";
+import React from "react";
+
+type UseCurrentUser = {
+  user: IUser;
+};
+
+export default function useCurrentUser(): UseCurrentUser {
+  return {
+    user: {
+      image: "/assets/user-images/image-zena.jpg",
+      name: "Zena Kelley",
+      username: "velvetround",
+    },
+  };
+}
