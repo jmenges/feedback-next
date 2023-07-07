b8af3c438c54671f34cce71f41920e58ad479e23
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-07 14:12:48 +0200

    Use real feedbacks from custom hook

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,23 +1,27 @@
+"use client";
+
 import RoadmapList from "@/components/RoadmapList";
 import RoadmapActionBar from "@/components/action-bar/RoadmapActionBar";
-import { initialFeedbacks } from "@/data/initialFeedbacks";
 import { roadmaps } from "@/data/roadmaps";
+import { useFeedbackStore } from "@/store/useFeedbackStore";
+import { useMemo } from "react";
 
 const validStatus = roadmaps.map((roadmap) => roadmap.title.toLowerCase());
-const feedbacksWithRelevantStatus = initialFeedbacks.filter((feedback) =>
-  validStatus.includes(feedback.status)
-);
 
 export default function Roadmap() {
+  const { feedbacks, upvoteFeedback } = useFeedbackStore();
+  const feedbacksWithRelevantStatus = useMemo(
+    () => feedbacks.filter((feedback) => validStatus.includes(feedback.status)),
+    [feedbacks]
+  );
+
   return (
     <div className="flex flex-wrap gap-6">
       <main className="flex-grow tablet:space-y-4">
         <header className="">
           <RoadmapActionBar />
         </header>
-        <div>
-          <RoadmapList feedbacks={feedbacksWithRelevantStatus} />
-        </div>
+        <RoadmapList upvoteFeedback={upvoteFeedback} feedbacks={feedbacksWithRelevantStatus} />
       </main>
     </div>
   );
