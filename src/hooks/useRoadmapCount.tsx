870cfe120dffc6fac1785363b22bf73af30cc093
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-07 14:09:13 +0200

    Create custom hook to count roadmaps

---
+++ src/hooks/useRoadmapCount.tsx
@@ -0,0 +1,42 @@
+import { IFeedback } from "@/types";
+import React, { useMemo } from "react";
+
+type Props = { feedbacks: IFeedback[] };
+
+type RoadmapCount = {
+  counts: {
+    title: string;
+    count: number;
+  }[];
+};
+
+export default function useRoadmapCount({ feedbacks }: Props): RoadmapCount {
+  const counts = useMemo(
+    () => [
+      {
+        title: "Planned",
+        count: feedbacks.reduce(
+          (acc, curr) => acc + Number(curr.status === "planned"),
+          0
+        ),
+      },
+      {
+        title: "In-Progress",
+        count: feedbacks.reduce(
+          (acc, curr) => acc + Number(curr.status === "in-progress"),
+          0
+        ),
+      },
+      {
+        title: "Live",
+        count: feedbacks.reduce(
+          (acc, curr) => acc + Number(curr.status === "live"),
+          0
+        ),
+      },
+    ],
+    [feedbacks]
+  );
+
+  return { counts };
+}
