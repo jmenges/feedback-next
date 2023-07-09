1d92eb4b4a8e6689ea91636a764cab25ad2a8d99
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-09 17:22:36 +0200

    Replace array of strings by array of objects

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,10 @@
-export const categories = [
-  "All",
-  "UI",
-  "UX",
-  "Enhancement",
-  "Bug",
-  "Feature",
+import { Categories } from "@/types/categories";
+
+export const categories: Categories = [
+  { name: "all", label: "All" },
+  { name: "ui", label: "UI" },
+  { name: "ux", label: "UX" },
+  { name: "enhancement", label: "Enhancement" },
+  { name: "bug", label: "Bug" },
+  { name: "feature", label: "Feature" },
 ] as const;
