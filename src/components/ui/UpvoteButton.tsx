1852a742780ccffac9cca8e7b2ce56f45da034ae
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 17:10:56 +0200

    Change upvote button to client component

--- src/components/ui/UpvoteButton.tsx
+++ src/components/ui/UpvoteButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 import UpIcon from "@/../public/icons/icon-arrow-up.svg";
@@ -18,8 +20,14 @@ export default function UpvoteButton({
   let sizeClass =
     size === "default" ? "tablet:flex-col tablet:gap-[6px] tablet:py-3.5" : "";
 
+  const onClick = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
   return (
     <Button
+      onClick={onClick}
       variant="interactive"
       className={cn(
         "flex h-auto items-center gap-[10px] py-1 font-bold text-darker-blue tablet:pb-[6px] tablet:pl-4 tablet:pr-[16.5px] tablet:pt-[7px] desktop:pb-[10px] desktop:pt-[11px]",
