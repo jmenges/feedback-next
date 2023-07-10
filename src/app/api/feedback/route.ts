5d9752e1f88786999890eb5edb303478f87b5767
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 12:25:31 +0200

    Add api handler for updating feedbacks

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,5 +1,5 @@
 import { Feedback } from "@/models/feedback";
-import { postFeedbackSchema } from "@/validations/feedback";
+import { patchFeedbackSchema, postFeedbackSchema } from "@/validations/feedback";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -31,3 +31,34 @@ export async function POST(req: NextRequest) {
   }
   return NextResponse.json({ message: "This Worked", success: true });
 }
+
+
+/* Edit feedback */
+/* TODO: add authentication */
+export async function PATCH(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const feedbackParsed = patchFeedbackSchema.parse(body);
+
+    /* Update feedback in database */
+    const isSuccess = await Feedback.update(feedbackParsed, 1);
+    if (!isSuccess) {
+      throw new Error("Failed to update feedback");
+    }
+  } catch (error) {
+    let errorMsg: string = "";
+    if (error instanceof z.ZodError) {
+      errorMsg = "Invalid data provided";
+      console.log(error.issues);
+    } else {
+      errorMsg = "An error occurred";
+      console.log(error);
+    }
+    return NextResponse.json(
+      { message: errorMsg, success: false },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ message: "This Worked", success: true });
+}
+
