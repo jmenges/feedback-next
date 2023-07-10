52fc2d0140a82c5bee205fe15a924cc974c7d07e
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 11:41:55 +0200

    Create api endpoint for adding feedbacks

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,33 @@
+import { Feedback } from "@/models/feedback";
+import { postFeedbackSchema } from "@/validations/feedback";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+/* Create new feedback */
+/* TODO: add authentication */
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const feedbackParsed = postFeedbackSchema.parse(body);
+
+    /* Insert feedback into database */
+    const isSuccess = await Feedback.add(feedbackParsed, 1);
+    if (!isSuccess) {
+      throw new Error("Failed to add feedback to db");
+    }
+  } catch (error) {
+    let errorMsg: string = "";
+    if (error instanceof z.ZodError) {
+      errorMsg = "Invalid data provided";
+      console.log(error.issues);
+    } else {
+      errorMsg = "An error occurred";
+      console.log(error);
+    }
+    return NextResponse.json(
+      { message: errorMsg, success: false },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ message: "This Worked", success: true });
+}
