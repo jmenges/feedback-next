699dfceafae8dc3ede5f333ae31e7a9223c3bf06
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 12:24:24 +0200

    Add validation for updating Feedbacks

--- src/validations/feedback.ts
+++ src/validations/feedback.ts
@@ -1,11 +1,26 @@
 import { categories } from "@/data/categories";
+import { status } from "@/data/status";
 import { z } from "zod";
 
 /* extract category values from categories */
-const categoryValues = categories.map(({value}) => value) as [string,...string[]];
+const categoryValues = categories.map(({ value }) => value) as [
+  string,
+  ...string[]
+];
 
 export const postFeedbackSchema = z.object({
   title: z.string(),
   description: z.string(),
   category: z.enum(categoryValues),
+});
+
+/* extract category values from categories */
+const statusValues = status.map(({ value }) => value) as [string, ...string[]];
+
+export const patchFeedbackSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  description: z.string(),
+  category: z.enum(categoryValues),
+  status: z.enum(statusValues),
 });
