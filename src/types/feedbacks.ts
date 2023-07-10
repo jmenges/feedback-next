54dd5cc9d5b772b9936cb029f223b11a9f8896b9
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 12:24:41 +0200

    Create FeedbackUpdate type

--- src/types/feedbacks.ts
+++ src/types/feedbacks.ts
@@ -12,3 +12,8 @@ export type FeedbackAdd = Pick<
   Prisma.FeedbackCreateInput,
   "title" | "category" | "description"
 >;
+
+export type FeedbackUpdate = { id: number } & Pick<
+  Prisma.FeedbackUpdateInput,
+  "title" | "category" | "description" | "status"
+>;
