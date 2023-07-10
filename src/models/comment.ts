1d175297a071cf9ecc4484b179c0fa7f6b06078c
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-10 17:13:36 +0200

    Add comment model

---
+++ src/models/comment.ts
@@ -0,0 +1,103 @@
+import { db } from "@/lib/server";
+import { CommentAdd, CommentPopulated } from "@/types/comments";
+import { Prisma } from "@prisma/client";
+
+export abstract class Comment {
+  static queryByFeedbackId = async (
+    feedbackId: number
+  ): Promise<CommentPopulated[] | null> => {
+    /* Execute query */
+    const comments = await db.comment.findMany({
+      include: commentPopulated,
+      where: {
+        feedbackId: feedbackId,
+      },
+    });
+    return comments;
+  };
+
+  static add = async ({
+    content,
+    authorId,
+    feedbackId,
+    replyingToCommentId,
+    replyingToUserId,
+  }: {
+    content: string;
+    authorId: number;
+    feedbackId: number;
+    replyingToCommentId?: number;
+    replyingToUserId?: number;
+  }): Promise<boolean> => {
+    try {
+      /* Handle normal comment */
+      if (replyingToCommentId === undefined) {
+        await db.comment.create({
+          data: {
+            content: content,
+            feedback: {
+              connect: {
+                id: feedbackId,
+              },
+            },
+            author: {
+              connect: {
+                id: authorId,
+              },
+            },
+          },
+        });
+      } else if (
+        replyingToUserId !== undefined &&
+        replyingToCommentId !== undefined
+      ) {
+        /* Handle reply */
+        await db.comment.create({
+          data: {
+            content: content,
+            feedback: {
+              connect: {
+                id: feedbackId,
+              },
+            },
+            replyingToComment: {
+              connect: {
+                id: replyingToCommentId,
+              },
+            },
+            replyingToUser: {
+              connect: {
+                id: replyingToUserId,
+              },
+            },
+            author: {
+              connect: {
+                id: authorId,
+              },
+            },
+          },
+        });
+      } else {
+        throw new Error("Invalid comment data");
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+}
+
+/**
+ * Prisma validators
+ */
+export const commentPopulated = Prisma.validator<Prisma.CommentInclude>()({
+  author: true,
+  replies: true,
+});
+
+export const replyPopulated = Prisma.validator<Prisma.CommentInclude>()({
+  author: true,
+  replyingToUser: true,
+});
