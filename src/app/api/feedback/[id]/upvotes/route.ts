186bf2cbfe4a5c146d3fd98d85f640b1bc30a884
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-11 12:16:01 +0200

    Add api handler for removing upvotes

--- src/app/api/feedback/[id]/upvotes/route.ts
+++ src/app/api/feedback/[id]/upvotes/route.ts
@@ -31,3 +31,31 @@ export async function POST(
   }
   return NextResponse.json({ message: "This Worked", success: true });
 }
+
+/* Remove upvote for authenticated user  */
+export async function DELETE(
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) {
+  const user = getServerUser();
+  const feedbackId = Number(id);
+
+  try {
+    /* Upvote feedback for authenticated user */
+    const isSuccess = await Feedback.removeUpvote({
+      id: feedbackId,
+      userId: user.id,
+    });
+    if (!isSuccess) {
+      throw new Error("Failed remove upvote of feedback");
+    }
+  } catch (error) {
+    const errorMsg = "An error occurred";
+    console.log(error);
+    return NextResponse.json(
+      { message: errorMsg, success: false },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ message: "This Worked", success: true });
+}
