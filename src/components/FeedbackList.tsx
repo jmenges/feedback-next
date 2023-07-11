c3695338fe6cbb5b5cb37239b74d46e9df344a08
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-11 11:03:21 +0200

    Remove upvote function based on old implementaion

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,25 +1,17 @@
-"use client";
-
 import FeedbackItem from "@/components/FeedbackItem";
 import { FeedbackPopulated } from "@/types/feedbacks";
 import Link from "next/link";
 
 type Props = {
   feedbacks: FeedbackPopulated[];
-  // upvoteFeedback: (feedbackId: number) => boolean;
 };
 
 export default function FeedbackList({ feedbacks }: Props) {
-  const upvoteFeedback = (feedbackId: number) => {return true}
-
   return (
     <div className="flex flex-col space-y-4 p-6 tablet:p-0">
       {feedbacks.map((feedback) => (
         <Link key={feedback.id} href={`/feedback/${feedback.id}`}>
-          <FeedbackItem
-            feedback={feedback}
-            upvoteFeedback={upvoteFeedback}
-          />
+          <FeedbackItem feedback={feedback}/>
         </Link>
       ))}
     </div>
