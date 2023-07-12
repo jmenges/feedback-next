79de43de1a399fd298bb531a001cef97fd9eff38
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-12 15:37:41 +0200

    Allow disabeling of button

--- src/components/ui/AddFeedbackButton.tsx
+++ src/components/ui/AddFeedbackButton.tsx
@@ -1,26 +1,44 @@
 import { cn } from "@/lib/utils";
 
-import { IconButtonProps } from "@/components/ui/IconButton";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export interface AddFeedbackButtonProps {
   className?: string;
+  disabled?: boolean;
+  disabledMessage?: string;
 }
 
 export default function AddFeedbackButton({
   className,
+  disabled = false,
+  disabledMessage = "",
 }: AddFeedbackButtonProps) {
+
   return (
-    <Link
-      className={cn(
-        className,
-        buttonVariants({ variant: "default" }),
-        "flex-shrink-0"
+    <>
+      {disabled === false ? (
+        <Link
+          className={cn(
+            className,
+            buttonVariants({ variant: "default" }),
+            "flex-shrink-0"
+          )}
+          href="/feedback/new"
+        >
+          + Add Feedback
+        </Link>
+      ) : (
+        <Button
+          className={cn(className, "flex-shrink-0 !pointer-events-auto")}
+          type="button"
+          variant="default"
+          title={disabledMessage}
+          disabled
+        >
+          + Add Feedback
+        </Button>
       )}
-      href="/feedback/new"
-    >
-      + Add Feedback
-    </Link>
+    </>
   );
 }
