f9a558eead667a98e65c0051f1d0319356dcd719
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-12 15:38:25 +0200

    Configure next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,31 @@
-import { db } from "@/lib/server";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+const prisma = new PrismaClient();
+const prismaAdapter = PrismaAdapter(prisma);
+
+// TODO: Fix types
+/**
+ * Overwrite standard createUser from prismaAdapter
+ * to map name provided by provider to username
+ */
+// @ts-ignore
+prismaAdapter.createUser = (data) => {
+  // we map name to username
+  const newUser = {
+    username: data.name,
+    email: data.email,
+    image: data.image,
+    emailVerified: data.emailVerified,
+  };
+
+  return prisma.user.create({
+    data: newUser,
+  });
+};
+
 export const authOptions = {
   providers: [
     GithubProvider({
@@ -9,6 +33,24 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  adapter: PrismaAdapter(db),
+  adapter: prismaAdapter,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.userId = user.id;
+        token.name = user.name;
+        token.image = user.image;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.userId;
+
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
