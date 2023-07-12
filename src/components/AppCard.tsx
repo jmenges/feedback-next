240a60636036155825197b964fcd8bac401a2667
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-12 15:40:46 +0200

    Change layout of AppCard

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -6,10 +6,10 @@ type Props = {};
 export default function AppCard({}: Props) {
 
   return (
-    <Card className="rounded-none w-full bg-app-card-mobile bg-cover p-4 tablet:rounded-md tablet:bg-app-card-tablet tablet:p-6 tablet:!pt-[62px] tablet:max-desktop:basis-1/3 desktop:bg-app-card-desktop">
-      <h2 className="text-white">Frontend Mentor</h2>
+    <Card className="rounded-none w-full flex flex-col bg-app-card-mobile bg-cover p-4 tablet:rounded-md tablet:bg-app-card-tablet tablet:p-6 tablet:max-desktop:basis-1/3 desktop:bg-app-card-desktop">
+      <UserActionButton className="ml-auto"/>
+      <h2 className="text-white tablet:!pt-[30px]">Frontend Mentor</h2>
       <p className="text-white opacity-75">Feedback Board</p>
-      <UserActionButton />
     </Card>
   );
 }
