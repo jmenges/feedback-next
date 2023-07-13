43d54852df07cfe47fb5a808310dd058bf1eac48
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-13 15:54:32 +0200

    Update types

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,15 +1,24 @@
 import FeedbackItem from "@/components/FeedbackItem";
-import { FeedbackPopulated } from "@/types/feedbacks";
+import { cn } from "@/lib/utils";
+import {
+  FeedbackPopulated,
+  FeedbackPopulatedAuthenticated,
+} from "@/types/feedbacks";
 import Link from "next/link";
 
 type Props = {
-  feedbacks: FeedbackPopulated[];
+  feedbacks: FeedbackPopulated[] | FeedbackPopulatedAuthenticated[];
   isAuthenticated: boolean;
+  className?: string;
 };
 
-export default function FeedbackList({ feedbacks, isAuthenticated }: Props) {
+export default function FeedbackList({
+  feedbacks,
+  isAuthenticated,
+  className,
+}: Props) {
   return (
-    <div className="flex flex-col space-y-4 p-6 tablet:p-0">
+    <div className={cn("flex flex-col space-y-4", className)}>
       {feedbacks.map((feedback) => (
         <Link key={feedback.id} href={`/feedback/${feedback.id}`}>
           <FeedbackItem feedback={feedback} isAuthenticated={isAuthenticated} />
