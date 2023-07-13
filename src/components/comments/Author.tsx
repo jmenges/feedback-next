8507eb269ff19eb09c3201894bf236710b051adb
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-13 23:25:49 +0200

    Update types

--- src/components/comments/Author.tsx
+++ src/components/comments/Author.tsx
@@ -1,18 +1,17 @@
-import { IUser } from "@/types/types";
-import React from "react";
+import { User } from "@prisma/client";
 
 type Props = {
-  author: IUser;
+  author: User;
 };
 
-export default function Author({ author: { name, username, image } }: Props) {
+export default function Author({ author: { username, image } }: Props) {
   return (
     <div className="flex items-center">
       {/* Avatar */}
-      <img src={image} alt={name} className="mr-4 tablet:mr-8 h-10 w-10 rounded-full" />
+      <img src={image || ""} alt={username} className="mr-4 tablet:mr-8 h-10 w-10 rounded-full" />
       {/* Name and twitter  */}
       <div className="text-xs tablet:text-h4">
-        <h4 className="font-bold">{name}</h4>
+        {/* <h4 className="font-bold">{name}</h4> */}
         <p>@{username}</p>
       </div>
     </div>
