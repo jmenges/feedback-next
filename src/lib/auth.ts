8214ab85e2f3a75bce416d60c86aea7e282a4c09
Jonas Menges <dev.menges.jonas@gmail.com>
2023-07-13 18:07:39 +0200

    Use credentialProvider for next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
+import { db } from "@/lib/server";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { loginSchema } from "@/validations/auth";
 
 const prisma = new PrismaClient();
 const prismaAdapter = PrismaAdapter(prisma);
@@ -22,15 +25,63 @@ prismaAdapter.createUser = (data) => {
   };
 
   return prisma.user.create({
-    data: newUser,
+    data: newUser
   });
 };
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth/login",
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        try {
+          const loginParsed = loginSchema.parse(credentials);
+          const user = await db.user.findFirst({
+            where: {
+              username: loginParsed.username,
+            },
+            include: {
+              accounts: {
+                where: {
+                  provider: "credentials",
+                },
+              },
+            },
+          });
+
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          if (user.accounts.length === 0) {
+            throw new Error("User not registered for login with password");
+          }
+
+          if (user.accounts[0].password !== loginParsed?.password) {
+            throw new Error("Invalid credentials");
+          }
+
+          // sanitize user object and return
+          //@ts-ignore
+          delete user.accounts;
+          return user;
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      },
     }),
   ],
   adapter: prismaAdapter,
@@ -41,16 +92,17 @@ export const authOptions = {
     async jwt({ token, user }) {
       if (user) {
         token.userId = user.id;
-        token.name = user.name;
+        token.name = user.username;
         token.image = user.image;
       }
+
       return token;
     },
     async session({ session, token }) {
       session.user.id = token.userId;
-
       return session;
     },
   },
 };
+
 export default NextAuth(authOptions);
